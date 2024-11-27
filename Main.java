d0d66ee1bb59d29afcc3d0164e6d36effaaf3256
Kyrillos-Hanna <kyrohanna90@gmail.com>
2024-11-27 10:06:13 -0800

    finished 4.2 hw

--- Main.java
+++ Main.java
@@ -4,6 +4,36 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter two numbers: ");
+    int num1 = sc.nextInt();
+    int num2 = sc.nextInt();
+    oddBetweenTwo(num1, num2);
+
+    System.out.println("Enter a positive integer: ");
+    int num3 = sc.nextInt();
+    placeValue(num3);
+  }
+
+  public static void oddBetweenTwo(int num1, int num2) {
+    while (num1 <= num2) {
+      if (num1 % 2 == 1) {
+        System.out.print(num1 + " ");
+      }
+      num1++;
+    }
+    System.out.println();
+  }
+
+  public static void placeValue(int num) {
+    int deg = 0;
+    while (num > 0) {
+      int digit = num % 10;
+      num /= 10;
+
+      double placeValue = Math.pow(10, deg);
+      System.out.println((int) (digit * placeValue));
+      deg++;
+    }
   }
 }
